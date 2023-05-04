5c1ed3915d4992539d53a5ca253fc32e3119cc2a
Nierd <eyeamnumreuno@gmail.com>
2023-05-04 13:11:06 -0600

    Began creating the composable for a timer. Final output will have a screend displaying several timer cards with a synched timer.

--- app/src/main/java/com/example/dotatimers/MainActivity.kt
+++ app/src/main/java/com/example/dotatimers/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.dotatimers
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dotatimers.ui.theme.DotaTimersTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +35,6 @@ class MainActivity : ComponentActivity() {
             DotaTimersTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
                 }
             }
         }
@@ -27,17 +42,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun Timer(firstWarning: Int, secondWarning: Int){
+    Surface(
+        modifier = Modifier.padding(8.dp),
+        shape = RoundedCornerShape(8.dp)
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ){
+            Box(
+                modifier = Modifier
+                    .height(120.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+            ){
+                Text(
+                    text = "100",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+            Spacer(modifier= Modifier.height(8.dp))
+            Button(
+                onClick = { placeholder() }
+            ){
+                Text(text = "Button")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+fun placeholder(){
+
+}
+
 @Composable
-fun GreetingPreview() {
-    DotaTimersTheme {
-        Greeting("Android")
-    }
+@Preview
+fun TimerPreview(){
+    Timer(10, 50)
 }
